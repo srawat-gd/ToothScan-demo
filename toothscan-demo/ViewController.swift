7b8cff0600a8d413654b8043ac706cc564362cc6
Shivam Rawat <shivam.rawat@mutualmobile.com>
2024-04-09 14:39:20 +0530

    Initial Commit

---
+++ toothscan-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toothscan-demo
+//
+//  Created by Shivam Rawat on 09/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
