8af5e6230200241124d82e7b2e218681e6eab746
srawat-gd <srawat@griddynamics.com>
2024-04-22 14:28:02 +0530

    Initial Commit

--- toothscan-demo/ViewController.swift
+++ toothscan-demo/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import SwiftUI
+import toothscan_sdk_ios
 
 class ViewController: UIViewController {
 
@@ -13,7 +15,36 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func tapOnButton(_ sender: Any) {
+    
+        class theme: IToothScanTheme {
+            var primaryColor: Color = .blue
+            var secondaryColor: Color = .yellow
+            var accentColor: Color = .red
+            var fontFamily: String =  "MagicMonolines"
+        }
+        
+        class secrets: IToothScanSecrets {
+            var partnerID: String = "partnerId"
+            var userID: String? = "userId"
+            var partnerSecret: String = "partnerSecret"
+        }
+        
+        class config: IToothScanConfigs {
+            var secrets: IToothScanSecrets?
+            var theme: IToothScanTheme?
+            init(secrets: IToothScanSecrets?, theme: IToothScanTheme?) {
+                self.secrets = secrets
+                self.theme = theme
+            }
+        }
+        
+        let toothscan = try? ToothScanSdk(config: config(secrets: secrets(), theme: theme()))
+        if let toothscan {
+            toothscan.displaySurvey()
+        }
+    }
 
 }
 
